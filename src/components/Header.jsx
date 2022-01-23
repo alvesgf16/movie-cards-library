758befd9b3fe1153933efea5b7bf7b506fc878a0
alvesgf16 <alvesgabriel1994@gmail.com>
2022-01-23 11:56:46 -0300

    style: change page title color to white

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 export default function Header() {
   return (
     <header className="flex justify-center items-center h-16 bg-gray-700">
-      <h1 className="text-2xl font-medium">Movie Cards Library</h1>
+      <h1 className="text-2xl font-medium text-white">Movie Cards Library</h1>
     </header>
   );
 }
