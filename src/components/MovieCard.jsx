5e04a8bebd6ee7035ad5c36ee331176ce0bd5028
alvesgf16 <alvesgabriel1994@gmail.com>
2022-01-23 11:58:35 -0300

    feat: display movie poster as card bgi

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -5,7 +5,7 @@ import fetchApi from '../services/fetchApi';
 
 export default function MovieCard({ movieId }) {
   const [movieInfo, setMovieInfo] = useState({});
-  const { image, title, genres, plot, imDbRating } = movieInfo;
+  const { image, title, genres, stars, imDbRating } = movieInfo;
 
   useEffect(() => {
     fetchApi(movieId, setMovieInfo);
@@ -16,15 +16,21 @@ export default function MovieCard({ movieId }) {
     <div
       className="flex flex-col justify-between border border-gray-400 m-2 border-solid w-72"
     >
-      <img
-        className="w-full h-40"
-        src={ image }
-        alt={ `${title} cover` }
-      />
-      <div className="space-y-4 h-72 p-5">
-        <h2 className="text-xl font-semibold">{ title }</h2>
-        <h3>{ genres }</h3>
-        <p className="text-sm text-justify">{ plot }</p>
+      <div
+        className="space-y-4 h-96 p-5 bg-center bg-cover"
+        style={{ backgroundImage: `url(${image})` }}
+      >
+        <h2
+          className="rounded-full text-xl font-semibold text-center bg-white bg-opacity-50 "
+        >
+          { title }
+        </h2>
+        <h3 className="rounded-full bg-white bg-opacity-50 p-2.5">{ genres }</h3>
+        <p
+          className="rounded-full text-sm text-justify bg-white bg-opacity-50 p-2.5"
+        >
+          { stars }
+        </p>
       </div>
       <div className="flex justify-end items-center h-10 px-3.5 bg-gray-500">
         <Rating rating={ imDbRating } />
